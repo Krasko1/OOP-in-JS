67c0d736b3951623970b04c24a40c7e6dc98b000
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-02-19 10:46:02 +0100

    Factories

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
-        console.log('draw');
+function createCircle(radius) {
+    return {
+        // radius: radius,
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
     }
-};
+}
 
-circle.draw();
+const circle = createCircle(1);
