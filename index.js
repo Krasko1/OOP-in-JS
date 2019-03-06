f57f504da53cb2c7e87af2443dc81c0a0f8c9abd
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-03-06 10:12:51 +0100

    Value vs reference types

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
+// let x = 10;
+// let y = x;
+
+// x = 20;
+
+// console.log(x);
+// console.log(y);
+
+
+// let x = { value: 10 };
+// let y = x;
+
+// x.value = 20;
+
+// console.log(x.value);
+// console.log(y.value);
+
+
+// let number = 10;
+
+// function increase(number){
+//     number++;
+// }
+// increase(number);
+
+// console.log(number);
+
+
+let obj = { value: 10 };
+
+function increase(obj){
+    obj.value++;
 }
+increase(obj);
 
-const circle = new Circle(1);
-
-Circle.call({}, 1);
-Circle.apply({}, [1, 2, 3]);
+console.log(obj.value);
