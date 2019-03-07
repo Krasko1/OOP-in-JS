7582e964dd49d59ede8c3f875181971b1cccac79
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-03-07 10:51:03 +0100

    Getters and setters

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ function Circle(radius){
         console.log('draw');
     }
 
+    this.getDefaultLocation = function(){
+        return defaultLocation;
+    }
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function(){
+            return defaultLocation;
+        },
+        set: function(value){
+            if(!x.value || !y.value)
+                throw new Error('Invalid location');
+
+            defaultLocation = value;
+        }
+    });
+
     let defaultLocation = {x: 0, y: 0};
     let computeOptimumLocation = function(){
     }
 }
 
 const circle = new Circle(10);
-// BAD:
-// circle.defaultLocation = false;
-// circle.computeOptimumLocation();
+
+// Not very good:
+// circle.getDefaultLocation();
