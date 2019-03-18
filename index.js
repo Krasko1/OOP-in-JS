8acf1e922c1b402009d5cee7225eedc533130604
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-03-18 09:23:42 +0100

    Avoid extending the built-in objects

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-function Circle(radius){
-    this.radius = radius;
+// Don't do this:
+Array.prototype.shuffle = function(){
+    // ...
+};
 
-    this.move = function(){
-        console.log('move');
-    }
-}
+const array = [];
 
-const c1 = new Circle(1);
-
-Circle.prototype.draw = function(){
-    console.log('draw');
-}
-
-c1.draw();
-
-// returns instance members
-console.log(Object.keys(c1));
-
-// returns all members (instance + prototype)
-for(let key in c1)
-    console.log(key);
+array.shuffle();
