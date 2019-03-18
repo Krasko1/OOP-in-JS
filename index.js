2d73a62065113505c6c39b9b62ee30e232ae2ed2
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-03-18 09:07:13 +0100

    Prototype vs instance members

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+// function Circle(radius){
+//     this.radius = radius;
+
+//     this.draw = function(){
+//         console.log('draw');
+//     }
+// }
+
+// const c1 = new Circle(1);
+// const c2 = new Circle(1);
+
 function Circle(radius){
+    // Instance members
     this.radius = radius;
+
+    this.move = function(){
+        this.draw();
+        console.log('move');
+    }
 }
 
-Circle.prototype
+// Prototype members
+Circle.prototype.draw = function(){
+    // this.move();
+    console.log('draw');
+}
 
-const circle = new Circle(1);
+const c1 = new Circle(1);
+const c2 = new Circle(1);
+
+Circle.prototype.toString = function(){
+    return 'Circle with radius ' + this.radius;
+}
