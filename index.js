311f1b8103e9728c60f2f5c3cf41df7d1344d397
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-03-25 08:44:46 +0100

    Creating my own prototypical inheritance

--- index.js
+++ index.js
@@ -1,52 +1,18 @@
-function Stopwatch(){
-    let startTime, endTime, running, duration;
+function Shape(){}
 
-    Object.defineProperty(this, 'duration',{
-        get: function(){ return duration; },
-        set: function(value) { duration = value; }
-    });
-    
-    Object.defineProperty(this, 'startTime',{
-        get: function(){ return startTime; }
-    });
-
-    Object.defineProperty(this, 'endTime',{
-        get: function(){ return endTime; }
-    });
-
-    Object.defineProperty(this, 'running',{
-        get: function(){ return running; }
-    });
+Shape.prototype.duplicate = function(){
+    console.log('duplicate');
 }
 
-Stopwatch.prototype.start = function(){
-    if(this.running){
-        throw new Error('Stopwatch has already been started.');
-    }
+function Circle(radius){
+    this.radius = radius;
+}
 
-    this.running = true; 
-    this.startTime = new Date();
-};
+Circle.prototype = Object.create(Shape.prototype);
 
-Stopwatch.prototype.stop = function() {
-    if(!this.running){
-        throw new Error('Stopwatch is not started.');
-    }
+Circle.prototype.draw = function(){
+    console.log('draw');
+}
 
-    this.running = false; 
-    this.endTime = new Date();
-
-    const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000;
-    this.duration += seconds;
-};
-
-Stopwatch.prototype.reset = function() {
-    this.startTime = null;
-    this.endTime = null;
-    this.running = false;
-    this.duration = 0;
-};
-
-const sw = new Stopwatch();
-// bad:
-sw.duration = 10;
+const s = new Shape();
+const c = new Circle(1);
