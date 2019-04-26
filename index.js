b2f81b579643d4b40cf4f47570acf20bfabb911a
Vladimir Krasko <vladimirkrasko@gmail.com>
2019-04-26 10:08:53 +0200

    Excercise Polymorphism

--- index.js
+++ index.js
@@ -18,7 +18,33 @@ function HtmlSelectElement(items = []){
     this.removeItem = function(item){
         this.items.splice(this.items.indexOf(item), 1);
     }
+
+    this.render = function(){
+        return `
+        <select>${this.items.map(item => `
+        <option>${item}</option>`).join('')}
+        </select>`;
+    }
 }
 
 HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+function HtmlImageElement(src){
+    this.src = src;
+
+    this.render = function(){
+        return `<img src="${this.src}" />`
+    }
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+const elements = [
+    new HtmlSelectElement([1, 2, 3]),
+    new HtmlImageElement('http://')
+];
+
+for(let element of elements)
+    console.log(element.render());
