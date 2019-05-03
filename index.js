f615ae32e642a31a77097dead8e7139706a62546
Krasko <vladimir.krasko@cyrillicsoftware.net>
2019-05-03 09:17:31 +0200

    Private members using weakmaps

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const _radius = Symbol();
-const _draw = Symbol();
+const _radius = new WeakMap();
+
+const _move = new WeakMap();
 
 class Circle{
     constructor(radius){
-        this[_radius] = radius;
+        _radius.set(this, radius);
+        // _move.set(this, function(){
+        //     console.log('move', this);
+        // });
+
+        _move.set(this, () => {
+            console.log('move', this);
+        });
     }
 
-    [_draw](){
+    draw(){
+        // console.log(_radius.get(this));
+        _move.get(this)();
+        console.log('draw');
     }
 }
 
 const c = new Circle(1);
-const key = Object.getOwnPropertySymbols(c)[0];
-console.log(c[key]);
