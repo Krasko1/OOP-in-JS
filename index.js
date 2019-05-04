b3f797e5a2852884921efa705b954bab8636b1a7
Krasko <vladimir.krasko@cyrillicsoftware.net>
2019-05-04 14:27:07 +0200

    Getters and setters

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const _radius = new WeakMap();
 
-const _move = new WeakMap();
-
 class Circle{
     constructor(radius){
         _radius.set(this, radius);
-        // _move.set(this, function(){
-        //     console.log('move', this);
-        // });
-
-        _move.set(this, () => {
-            console.log('move', this);
-        });
     }
 
-    draw(){
-        // console.log(_radius.get(this));
-        _move.get(this)();
-        console.log('draw');
+    // getRadius(){
+    //     return _radius.get(this);
+    // }
+
+    get radius(){
+        return _radius.get(this);
+    }
+
+    set radius(value){
+        if(value <= 0) throw new Error('Invalid radius');
+
+        _radius.set(this, value);
     }
 }
 
