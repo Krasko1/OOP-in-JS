f6a6ade23da7f30caa438cdd0494b449fd4c39bd
Krasko <vladimir.krasko@cyrillicsoftware.net>
2019-05-06 08:40:23 +0200

    Method riding

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 class Shape{
-    constructor(color){
-        this.color = color;
-    }
-
     move(){
         console.log('move');
     }
 }
 
 class Circle extends Shape{
-    constructor(color, radius){
-        super(color);
-        this.radius = radius;
-    }
-
-    draw(){
-        console.log('draw');
+    move(){
+        super.move();
+        console.log('circle move');
     }
 }
 
-const c = new Circle('red', 1);
+const c = new Circle();
